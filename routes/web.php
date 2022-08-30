70401b89ca152bed0289f18a28aa8fbca1decbbc
Pedro Rocha <pedrorocha.bou@gmail.com>
2022-08-30 18:12:57 -0300

    Configurando rotas
    Neste commit foram configuradas as rotas para a página principal e para
    a página de contato.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
+
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
